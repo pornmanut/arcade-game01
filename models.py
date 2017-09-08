59b3ed7a61ed8b6723a76afcb763821d56ca57fd
oorepenworkoo <oorepenworkoo@gmail.com>
2017-09-08 12:06:12 +0700

    add some advmoving

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+import arcade.key
 class World:
     def __init__(self, width, height):
         self.width = width
@@ -9,13 +10,47 @@ class World:
     def update(self, delta):
         self.ship.update(delta)
 
+    def on_key_press(self, key, key_modifiers):
+        if key == arcade.key.UP:
+            self.ship.DIRECTION = 1
+        if key == arcade.key.DOWN:
+            self.ship.DIRECTION = 3
+        if key == arcade.key.LEFT:
+            self.ship.DIRECTION = 0
+        if key == arcade.key.RIGHT:
+            self.ship.DIRECTION = 2
+
 class Ship:
-    Speed = 3
+    DIRECTION = 1
+    #0 left
+    #1 up
+    #2 right
+    #3 down
+    SPEED = 3
+
     def __init__(self,world,x,y):
         self.world = world
         self.x = x
         self.y = y
+        self.angle = 0
+
+    def switch_direction(self):
+        if(self.DIRECTION == 0):
+            self.angle = 90
+        elif(self.DIRECTION == 1):
+            self.angle = 0
+        elif(self.DIRECTION == 2):
+            self.angle = -90
+        else:
+            self.angle = 180
+
     def update(self,delta):
-        if(self.y) > self.world.height :
-            self.y = 0
-        self.y += Ship.Speed
+        self.switch_direction()
+        if(self.DIRECTION):
+            if(self.y > self.world.height):
+                self.y = 0
+            self.y += Ship.SPEED
+        else:
+            if(self.x > self.world.width):
+                self.x = 0
+            self.x += Ship.SPEED
--- space.py
+++ space.py
@@ -1,10 +1,29 @@
 import arcade
+
 from models import *
 
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
 
+class ModelSprite(arcade.Sprite):
+    def __init__(self, *args, **kwargs):
+        self.model = kwargs.pop('model', None)
+
+        super().__init__(*args, **kwargs)
+
+    def sync_with_model(self):
+        if self.model:
+            self.set_position(self.model.x, self.model.y)
+            self.angle = self.model.angle
+
+    def draw(self):
+        self.sync_with_model()
+        super().draw()
+
 class SpaceGameWindow(arcade.Window):
+
+    def on_key_press(self, key, key_modifiers):
+        self.world.on_key_press(key, key_modifiers)
 
     def __init__(self, width, height):
         super().__init__(width, height)
@@ -13,6 +32,7 @@ class SpaceGameWindow(arcade.Window):
         self.ship_sprite = arcade.Sprite('images/ship.png')
 
         self.world = World(width,height)
+        self.ship_sprite = ModelSprite('images/ship.png',model=self.world.ship)
 
     def on_draw(self):
         arcade.start_render()
@@ -23,5 +43,5 @@ class SpaceGameWindow(arcade.Window):
         self.ship_sprite.set_position(self.world.ship.x,self.world.ship.y)
 
 if __name__ == '__main__':
-    window = SpaceGameWindow(int(SCREEN_WIDTH/2), SCREEN_HEIGHT)
+    window = SpaceGameWindow(int(SCREEN_WIDTH*2), SCREEN_HEIGHT)
     arcade.run()
