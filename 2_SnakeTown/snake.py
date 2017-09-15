734703b60d5c1b54481cc627c7a868b7965f0baf
pornmanut <pornmanuty@gmail.com>
2017-09-15 10:12:07 +0700

    moving block snake

--- 2_SnakeTown/snake.py
+++ 2_SnakeTown/snake.py
@@ -1,20 +1,39 @@
 import arcade
-
+from models import *
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
+
+class ModelSprite(arcade.Sprite):
+    def __init__(self,*args,**kwargs):
+        self.model = kwargs.pop('model',None)
+        super().__init__(*args,**kwargs)
+    def sync_with_model(self):
+        if(self.model):
+            self.set_position(self.model.x,self.model.y)
+    def draw(self):
+        self.sync_with_model()
+        super().draw()
 
 class snakeWindow(arcade.Window):
     def __init__(self,width,height):
         super().__init__(width,height)
         arcade.set_background_color(arcade.color.RICH_BRILLIANT_LAVENDER)
-        self.snake_sprite = arcade.Sprite('images/block.png')
+
+        self.world = World(SCREEN_WIDTH,SCREEN_HEIGHT)
+        self.snake_sprite = ModelSprite('images/block.png',model=self.world.snake)
         self.snake_sprite.set_position(300,300)
+
+    def update(self,delta):
+        self.world.update(delta)
+
     def on_draw(self):
         arcade.start_render()
         self.snake_sprite.draw()
 
-
-if __name__ == '__main__':
+def main():
     window = snakeWindow(SCREEN_WIDTH,SCREEN_HEIGHT)
     arcade.set_window(window)
     arcade.run()
+
+if __name__ == '__main__':
+    main()
