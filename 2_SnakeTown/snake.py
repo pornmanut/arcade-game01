7a99a34c83c8f1859d66c7e9f06017835516a987
pornmanut <pornmanuty@gmail.com>
2017-09-15 09:50:45 +0700

    add block.png && add on_draw() in snakeTown

--- 2_SnakeTown/snake.py
+++ 2_SnakeTown/snake.py
@@ -6,7 +6,13 @@ SCREEN_HEIGHT = 600
 class snakeWindow(arcade.Window):
     def __init__(self,width,height):
         super().__init__(width,height)
-        arcade.set_background_color(arcade.color.AIR_FORCE_BLUE)
+        arcade.set_background_color(arcade.color.RICH_BRILLIANT_LAVENDER)
+        self.snake_sprite = arcade.Sprite('images/block.png')
+        self.snake_sprite.set_position(300,300)
+    def on_draw(self):
+        arcade.start_render()
+        self.snake_sprite.draw()
+
 
 if __name__ == '__main__':
     window = snakeWindow(SCREEN_WIDTH,SCREEN_HEIGHT)
