c004db0600f35c998a7ef0d203bf430ef7b9c2f2
pornmanut <pornmanuty@gmail.com>
2017-09-15 10:32:55 +0700

    add function that can change direction

--- 2_SnakeTown/models.py
+++ 2_SnakeTown/models.py
@@ -1,10 +1,23 @@
+DIR_UP = 1
+DIR_RIGHT = 2
+DIR_DOWN = 3
+DIR_LEFT = 4
+
+DIR_OFFSET = {DIR_UP:(0,1),
+              DIR_RIGHT:(1,0),
+              DIR_DOWN:(0,-1),
+              DIR_LEFT:(-1,0)}
+
 class Snake:
+    BLOCK_SIZE = 16
     MOVE_WAIT = 0.2
+    MOVE_SPEED = 16
     def __init__(self,world,x,y):
         self.world = world
         self.x = x
         self.y = y
 
+        self.direction = DIR_RIGHT
         self.wait_time = 0
 
     def update(self,delta):
@@ -15,7 +28,8 @@ class Snake:
 
         if(self.x > self.world.width):
             self.x = 0
-        self.x += 16
+        self.x += self.MOVE_SPEED*DIR_OFFSET[self.direction][0]
+        self.y += self.MOVE_SPEED*DIR_OFFSET[self.direction][1]
         self.wait_time = 0
 
 class World:
